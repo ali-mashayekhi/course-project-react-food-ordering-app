ba5e30c1da6e0276aa9e9a81643f6c7c313f6105
ali-mashayekhi <alimashayekhi42@gmail.com>
2023-12-13 09:33:29 +0330

    getting meals data from api completed

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,9 +1,56 @@
 import styles from "./AvailableMeals.module.css";
 import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
+import { useEffect, useState } from "react";
 
 function AvailableMeals() {
-  const mealsList = DUMMY_MEALS.map((meal) => (
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
+
+  useEffect(() => {
+    async function getMeals() {
+      try {
+        const response = await fetch(
+          "https://react-course-a68bc-default-rtdb.firebaseio.com/meals.json"
+        );
+        if (!response.ok) throw new Error("Something went wrong");
+        const meals = await response.json();
+        const newArr = [];
+        const mealIds = Object.keys(meals);
+        mealIds.forEach((id) => {
+          newArr.push({ ...meals[id], id });
+        });
+        setMeals(newArr);
+        setIsLoading(false);
+        setHttpError(false);
+      } catch (error) {
+        console.log(error);
+        setHttpError(error.message);
+        setIsLoading(false);
+      }
+    }
+
+    getMeals();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section className={styles.mealsLoading}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section className={styles.mealsError}>
+        <p>{httpError}</p>
+      </section>
+    );
+  }
+
+  const mealsList = meals.map((meal) => (
     <MealItem
       name={meal.name}
       description={meal.description}
